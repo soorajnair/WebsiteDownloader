33746ab45bc90157ba7d0d831b1e5359f82e4f7e
Sooraj Suresh Babu <Sooraj.SureshBabu@csscorp.com>
2017-10-03 11:23:22 +0530

    deleted chrome comment

--- src/Browser/ExcessBrowser.java
+++ src/Browser/ExcessBrowser.java
@@ -1,10 +1,12 @@
 package Browser;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import java.io.*;
 public class ExcessBrowser {
-	
-	String URL[];
+	String baseTab;
+	String[] URL= new String[12];
 	//public static String DriverPath= "C:\\Users\\sooraj\\workspace\\WebsiteDownloader\\ChromeDriver\\chromedriver.exe" ;
 	public static String DriverPathIE= "\\Users\\css109558\\workspace\\WebsiteDownloader\\src\\Browser\\IE\\IEDriverServer.exe" ;
 	public static WebDriver driver;
@@ -15,9 +17,9 @@ public class ExcessBrowser {
 				System.out.println("Enter URL");
 				System.out.println("Enter 'q' to quit.");
 				try{
-					for(int i=0;i<10 || URL[i]!="q";i++ )
-				URL[i] = br.readLine();
-				//} while(!URL.equals("q"));
+					URL[0]="www.google.com";
+					for(int i=0;URL[i]!=null;i++ )
+					{	URL[i] = br.readLine();}
 					
 				   }
 				catch(Exception e)
@@ -34,7 +36,12 @@ public class ExcessBrowser {
 		System.setProperty("webdriver.ie.driver",DriverPathIE);
 		driver = new InternetExplorerDriver();
 		for(int i=0;i<MyURL.length;i++)
-			driver.navigate().to(MyURL[i]);
+		    {
+			//driver.navigate().to(MyURL[i]);
+			//String baseTab = driver.getWindowHandle();
+			driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "t");
+			driver.get(MyURL[i]); 
+			}
 		}
 		catch(Exception e)
 		{
